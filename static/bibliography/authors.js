b64c081c95ebed5e350fc767740a5393c05a1ae5
Kui Xu <xukui.cs@gmail.com>
2022-02-19 15:04:21 +0800

    add author link

---
+++ static/bibliography/authors.js
@@ -0,0 +1,21 @@
+json = '{\
+    "Qiangfeng Cliff Zhang": "http://zhanglab.net/team?index=1",\
+    "Xiaogang Wang": "http://www.ee.cuhk.edu.hk/~xgwang/",\
+    "Yuxin Peng": "http://59.108.48.34/tiki/yuxinpeng/",\
+    "Hongsheng Li": "https://www.ee.cuhk.edu.hk/~hsli/",\
+    "Jianping Shi":"http://shijianping.me",\
+    "Zhe Wang": "https://wang-zhe.me/",\
+    "Lei Sun":"https://scholar.google.com/citations?user=qdB0PMYAAAAJ&hl=zh-CN",\
+    "Pan Li":"https://scholar.google.com/citations?user=WsgiJJIAAAAJ&hl=zh-CN",\
+    "Yigong Shi":"https://ygshi.org/people",\
+    "Ruixue Wan":"http://www.icsb.tsinghua.edu.cn/english.php?c=show&id=510",\
+    "Hong-Wei Wang":"http://cryoem.life.tsinghua.edu.cn/?page_id=1104",\
+    "Nan Liu":"https://scholar.google.com/citations?user=jkL6NhgAAAAJ&hl=zh-CN",\
+    "Lei Xiong":"http://xiong-lei.com/",\
+    "Hongwei Yi":"https://scholar.google.com/citations?user=ocMf7fQAAAAJ&hl=zh-CN",\
+    "Hui Zhou":"https://scholar.google.com/citations?hl=zh-CN&user=i35tdbMAAAAJ",\
+    "Yanan Chen":"https://scholar.google.com/citations?user=5tDbQP8AAAAJ&hl=zh-CN",\
+    "Jun Xu":"https://csjunxu.github.io/",\
+    "Jing Gong": "https://tsinghua-gongjing.github.io/about.html"\
+}';
+const authors_link = JSON.parse(json);
